7298df7096fb214b737d470b8381a9ecfa69e436
Ganna004 <Gannafirst@gmail.com>
2026-08-24 00:42:29 +0300

    feat: add shopping list
    Add shopping list functionality generated from recipe ingredients. Users can add recipe ingredients to the shopping list, view the items, and mark items as completed.

---
+++ app/src/main/java/com/example/platemate/screens/ShoppingListScreen.kt
@@ -0,0 +1,52 @@
+package com.example.platemate.screens
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.platemate.domain.model.ShoppingListItem
+
+@Composable
+fun ShoppingListScreen(
+    items: List<ShoppingListItem>,
+    onItemClick:(ShoppingListItem) -> Unit,
+    onBackClick:() -> Unit
+)
+{
+    Column( modifier = Modifier
+        .fillMaxSize()
+        .padding(16.dp),
+        verticalArrangement= Arrangement.spacedBy(12.dp)
+    )
+    {
+    Text("Shopping List \uD83D\uDED2")
+        if (items.isEmpty())
+        {
+            Text("Your shopping list is empty.")
+        } else
+        {
+            items.forEach { item ->
+                Text(
+                    text = if(item.isChecked){
+                        "☑ ${item.name}" + (item.amount?.let { " - $it" } ?: "")
+                    } else
+                    {
+                        "☐ ${item.name}" + (item.amount?.let { " - $it" } ?: "")
+                    },
+                    modifier = Modifier.clickable{
+                        onItemClick(item)
+                    }
+                )
+            }
+        }
+        Button(onClick = onBackClick) {
+            Text("Back")
+        }
+    }
+}
